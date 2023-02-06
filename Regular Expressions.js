5cee6c9dc71880bcfb4330aab82b266c7bd82587
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-02-06 19:40:12 +0300

    Update Regular Expressions.js

--- Regular Expressions.js
+++ Regular Expressions.js
@@ -6,3 +6,8 @@ let myRegex = /Hello/;
 let result = myRegex.test(myString); // Change this line
 ________________________________________
 
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo is hiding/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
+________________________________________
+
