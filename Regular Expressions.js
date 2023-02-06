ee140a1a0153bbce1b6254325a1d2e6ebdc7e8b9
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-02-06 19:42:24 +0300

    Update Regular Expressions.js

--- Regular Expressions.js
+++ Regular Expressions.js
@@ -11,3 +11,8 @@ let waldoRegex = /Waldo is hiding/; // Change this line
 let result = waldoRegex.test(waldoIsHiding);
 ________________________________________
 
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
+________________________________________
+
