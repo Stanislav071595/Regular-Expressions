0b1bfa38f5a0b07335fd275dc3f8918ba05a7428
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-02-06 19:38:32 +0300

    Update Regular Expressions.js

--- Regular Expressions.js
+++ Regular Expressions.js
@@ -1,2 +1,8 @@
 /* Regular Expressions */
 ________________________________________
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
+________________________________________
+
