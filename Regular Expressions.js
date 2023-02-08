17b340fcc3e25cd0c86e18e2f6b36a4b14c8c65f
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-02-08 10:26:39 +0300

    Update Regular Expressions.js

--- Regular Expressions.js
+++ Regular Expressions.js
@@ -91,3 +91,8 @@ let alphabetRegexV2 = /\w/ig; // Change this line
 let result = quoteSample.match(alphabetRegexV2).length;
 ________________________________________
 
+let quoteSample = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /\W/ig; // Change this line
+let result = quoteSample.match(nonAlphabetRegex).length;
+________________________________________
+
